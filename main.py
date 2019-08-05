4853dd4cb0f3c89d0072957bc2c3c40b60415ab0
Avi0n <avion@pm.me>
2019-08-05 16:23:29 -0700

    Added support for song.link URLs

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from __future__ import unicode_literals
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+from time import sleep
 from telegram.ext import (MessageHandler, CommandHandler, BaseFilter, Updater)
 import os
 import logging
 import youtube_dl
+import requests
 
 
 # Initialize dotenv
@@ -13,9 +16,40 @@ mp3_name = ''
 
 # Initialize logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.DEBUG)
+                    level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+# Find YouTube/Soundcloud link from song.link URLs
+def scrape_songlink(url):
+    url = url
+
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    # Returns a requests.models.Response object
+    page = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    # Find all links
+    link_found = False
+    while link_found is False:
+        for link in soup.find_all('a'):
+            if 'youtube.com' in link.get('href'):
+                print(link.get('href'))
+                link_found = True
+                return link.get('href')
+            elif 'soundcloud.com' in link.get('href'):
+                print(link.get('href'))
+                link_found = True
+                return link.get('href')
+        print("Couldn't find a YouTube/Soundcloud URL to download from")
 
 
 # Respond to /start
@@ -25,10 +59,6 @@ def start(bot, update):
 
 
 # YouTube-dl stuff
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-
 class MyLogger(object):
     def debug(self, msg):
         pass
@@ -39,12 +69,13 @@ class MyLogger(object):
     def error(self, msg):
         print(msg)
 
+
 def my_hook(d):
     if d['status'] == 'finished':
         print('Done downloading, now converting ...')
 
+
 def download_audio(url):
-    print("entered download_audio()")
     global mp3_name
     ydl_opts = {
         'outtmpl': '%(title)s.%(ext)s',
@@ -58,9 +89,10 @@ def download_audio(url):
         'progress_hooks': [my_hook],
     }
 
+    print('Downloading video and/or audio')
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
-    
+
     # Find filename of converted audio
     for fname in os.listdir('.'):
         if fname.endswith('.mp3'):
@@ -68,24 +100,59 @@ def download_audio(url):
             os.rename(fname, 'song.mp3')
             break
 
+
 def delete_audio():
     # Cleanup downloaded media
     for fname in os.listdir('.'):
         if fname.endswith('.mp3'):
             os.remove(fname)
-# End of YouTube-dl stuff
+
+# End of YouTube-dl stuffError
 
 
-# Telegram bot
+def check_message_for_link(bot, update):
+    # Assign link in message or sticker to a variable
+    media_url=''
+
+    if not update.message.text is None:
+        media_url=update.message.text
+        print(media_url)
+
+    if 'song.link' in update.message.text:
+        # Grab YouTube/Soundcloud URL from song.link URL
+        media_url=scrape_songlink(update.message.text)
+    else:
+        print('Not a song.link URL, continuing to download song.')
+
+    download_audio(media_url)
+
+    # Try to send telegram message with audio file. If error, try again in 5 sec.
+    i = 0
+    while i < 5:
+        i += 1
+        try:
+            bot.send_audio(chat_id=update.message.chat_id, audio=open(
+                           './song.mp3', 'rb'), title=mp3_name, timeout=20)
+            print('Telegram message sent!')
+            break
+        except Exception as e:
+            print('Exception: ' + e + '. Trying again in 5 seconds')
+            sleep(5)
+            continue
+
+    # All done! Delete audio file.
+    delete_audio()
+
+
 class FilterLinks(BaseFilter):
     def filter(self, message):
-        accepted_links = [
-            "youtube.com",
-            "youtu.be",
-            "soundcloud.com",
-            "song.link"
+        accepted_links=[
+            'youtube.com',
+            'youtu.be',
+            'soundcloud.com',
+            'song.link'
         ]
-        media_url = message.text
+        media_url=message.text
 
         for i in accepted_links:
             if i and 'http' in media_url:
@@ -93,35 +160,20 @@ class FilterLinks(BaseFilter):
                 return True
 
 
-def check_message_for_link(bot, update):
-    # Assign link in message or sticker to a variable
-    media_url = ""
-
-    if not update.message.text is None:
-        media_url = update.message.text
-        print(media_url)
-
-    #if update.message.chat.title == "Bot testing" or update.message.chat.title == "Music":
-        # If message contains :heart:, add 3 points and forward the message to whoever liked it
-    download_audio(media_url)
-    bot.send_audio(chat_id=update.message.chat_id, audio=open('./song.mp3', 'rb'), title=mp3_name, timeout=20)
-    delete_audio()
-
-
 def main():
     # Initialize link filter class
-    filter_links = FilterLinks()
+    filter_links=FilterLinks()
 
     # Set Chris bot token
-    updater = Updater(token=os.getenv("TEL_BOT_TOKEN"), request_kwargs={
+    updater=Updater(token=os.getenv('TEL_BOT_TOKEN'), request_kwargs={
                       'read_timeout': 15, 'connect_timeout': 30})
 
-    dispatcher = updater.dispatcher
+    dispatcher=updater.dispatcher
 
-    start_handler = CommandHandler('start', start)
+    start_handler=CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
 
-    message_handler = MessageHandler(filter_links, check_message_for_link)
+    message_handler=MessageHandler(filter_links, check_message_for_link)
     dispatcher.add_handler(message_handler)
 
     dispatcher.add_error_handler(error)
