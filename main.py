908e95f9b52ad01f3062568d3a717b6769487be8
Avi0n <avion@pm.me>
2020-01-03 22:15:29 -0800

    Testing change to API

--- main.py
+++ main.py
@@ -28,35 +28,39 @@ def error(update, context):
 
 
 # Find YouTube/Soundcloud link from song.link URLs
-def scrape_songlink(url):
+def get_song_download_url(url):
+    youtube_url = ''
+    soundcloud_url = ''
     global songlink_url
 
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    API_URL = "https://api.song.link/v1-alpha.1/links?"
 
-    # Returns a requests.models.Response object
-    page = requests.get(url, headers=headers)
+    data = {
+        "url": "https://open.spotify.com/track/6pnVfCD8pWUb6GSu5OlBDn?si=ie6_8Xc1RlK1mrgPttvGyg",
+        "userCountry": "US"
+    }
 
-    songlink_url = url
+    response = requests.get(API_URL, data)
+    json_data = response.json()
 
-    soup = BeautifulSoup(page.text, 'html.parser')
+    youtube_url = json_data['linksByPlatform']['youtube']['url']
+    soundcloud_url = json_data['linksByPlatform']['soundcloud']['url']
 
-    # Find all links
-    link_found = False
-    while link_found is False:
-        for link in soup.find_all('a'):
-            if 'youtube.com' in link.get('href'):
-                print(link.get('href'))
-                return link.get('href')
-            elif 'soundcloud.com' in link.get('href'):
-                print(link.get('href'))
-                return link.get('href')
+    if youtube_url is not None:
+        print(youtube_url)
+        return youtube_url
+    elif soundcloud_url is not None:
+        print(soundcloud_url)
+        return soundcloud_url
+    else:
         print("Couldn't find a YouTube/Soundcloud URL to download from")
-        return
+    return
+
 
 # Respond to /start
 def start(context, update):
     context.bot.send_message(chat_id=update.message.chat_id,
-                     text="This bot can send you .mp3's of videos.")
+                             text="This bot can send you .mp3's of videos.")
 
 
 # YouTube-dl stuff
@@ -78,8 +82,8 @@ def my_hook(d):
 
 def download_audio(url):
     global mp3_name
+
     ydl_opts = {
-        'outtmpl': '%(title)s.%(ext)s',
         'format': 'bestaudio/best',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
@@ -89,6 +93,8 @@ def download_audio(url):
         'logger': MyLogger(),
         'progress_hooks': [my_hook],
     }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
 
     print('Downloading video and/or audio')
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -113,34 +119,22 @@ def delete_audio():
 
 def check_message_for_link(update, context):
     # Assign link in message or sticker to a variable
-    media_url=''
+    media_url = ''
 
-    if not update.message.text is None:
-        media_url=update.message.text
+    if update.message.text is not None:
+        media_url = get_song_download_url(update.message.text)
         print(media_url)
-
-    # Grab YouTube/Soundcloud link from Song.link if it's a spotify or song.link URL
-    if 'spotify.com' in update.message.text:
-        print('song.linkifying spotify link')
-        media_url=scrape_songlink('https://odesli.co/' + update.message.text)
-    elif 'music.apple.com' in update.message.text:
-        print('song.linkifying Apple Music link')
-        media_url=scrape_songlink('https://odesli.co/' + update.message.text)        
-    elif 'song.link' in update.message.text:
-        # Grab YouTube/Soundcloud URL from song.link URL
-        media_url=scrape_songlink(update.message.text)
-    else:
-        print('Not a song.link or spotify/apple URL, continuing to download song.')
 
     # Download video/audio and covert to mp3
     try:
+        print('download function url: ' + media_url)
         download_audio(media_url)
         # Get song.link URL
-        media_url=scrape_songlink('https://odesli.co/' + update.message.text)
+        media_url = get_song_download_url('https://odesli.co/' + update.message.text)
     except Exception as e:
         print('Exception: ' + str(e))
         context.bot.send_message(chat_id=update.message.chat_id,
-                    text="Couldn't find a YouTube/Soundcloud URL to download from.")
+                                 text="Couldn't find a YouTube/Soundcloud URL to download from.")
 
     # Try to send telegram message with audio file. If error, try again in 5 sec.
     i = 0
@@ -149,9 +143,9 @@ def check_message_for_link(update, context):
         i += 1
         try:
             context.bot.send_audio(chat_id=update.message.chat_id, audio=open(
-                           './song.mp3', 'rb'), title=mp3_name, disable_notification=True, 
-                           caption='[Song.link URL](' + songlink_url + ')', 
-                           parse_mode='Markdown', timeout=20)
+                './song.mp3', 'rb'), title=mp3_name, disable_notification=True,
+                caption='[Song.link URL](' + songlink_url + ')',
+                parse_mode='Markdown', timeout=20)
             print('Telegram message sent!')
             break
         except Exception as e:
@@ -159,8 +153,9 @@ def check_message_for_link(update, context):
             print('Exception: ' + str(e) + '. Trying again in 5 seconds')
             sleep(5)
             continue
-    if i is 5:
-        context.bot.send_message(chat_id=update.message.chat_id, text=error, disable_notification=True)
+    if i == 5:
+        context.bot.send_message(
+            chat_id=update.message.chat_id, text=error, disable_notification=True)
 
     # All done! Delete audio file.
     delete_audio()
@@ -168,7 +163,7 @@ def check_message_for_link(update, context):
 
 class FilterLinks(BaseFilter):
     def filter(self, message):
-        accepted_links=[
+        accepted_links = [
             'youtube.com',
             'youtu.be',
             'soundcloud.com',
@@ -177,7 +172,7 @@ class FilterLinks(BaseFilter):
             'coub.com',
             'song.link'
         ]
-        media_url=message.text
+        media_url = message.text
 
         for i in accepted_links:
             if i in media_url:
@@ -187,17 +182,17 @@ class FilterLinks(BaseFilter):
 
 def main():
     # Initialize link filter class
-    filter_links=FilterLinks()
+    filter_links = FilterLinks()
 
     # Set Chris bot token
     updater = Updater(os.getenv('TEL_BOT_TOKEN'), use_context=True)
 
-    dispatcher=updater.dispatcher
+    dispatcher = updater.dispatcher
 
-    start_handler=CommandHandler('start', start)
+    start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
 
-    message_handler=MessageHandler(filter_links, check_message_for_link)
+    message_handler = MessageHandler(filter_links, check_message_for_link)
     dispatcher.add_handler(message_handler)
 
     dispatcher.add_error_handler(error)
